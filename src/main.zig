5761bdbb223364f4ae99ec4ee3767e887115136b
Lorenzo Susini <susinilorenzo1@gmail.com>
2025-05-24 09:34:20 +0000

    fork, execve, chroot

--- src/main.zig
+++ src/main.zig
@@ -2,45 +2,36 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
+const linux = @import("std").os.linux;
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zunner_lib");
+// linux.getErrno(value) != .SUCCESS
+
+pub fn main() !void {
+    const bin = "/bin/sh";
+    const argv: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{ bin, "-i" };
+    const envp: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{};
+    const newRoot = "/tmp/alpine";
+
+    var ret = linux.fork();
+    if (linux.E.init(ret) != .SUCCESS) {
+        std.debug.panic("panic\n", .{});
+    }
+
+    if (ret != 0) { // parent
+        std.debug.print("{}\n", .{ret});
+        var status: u32 = undefined;
+        ret = linux.waitpid(-1, &status, 0);
+        if (linux.E.init(ret) != .SUCCESS) {
+            std.debug.panic("waitpid failed\n", .{});
+        }
+    } else { // child
+        ret = linux.chroot(newRoot);
+        if (linux.E.init(ret) != .SUCCESS) {
+            std.debug.panic("chroot failed\n", .{});
+        }
+        std.debug.print("hey\n", .{});
+        _ = linux.execve(bin, argv, envp);
+        std.debug.print("panic", .{});
+    }
+}
