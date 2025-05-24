90669d460e115479017e194b96ae9566d275f377
Lorenzo Susini <susinilorenzo1@gmail.com>
2025-05-24 12:08:01 +0000

    clone

--- src/main.zig
+++ src/main.zig
@@ -3,35 +3,46 @@
 //! is to delete this file and start with root.zig instead.
 
 const std = @import("std");
-const linux = @import("std").os.linux;
+const linux = std.os.linux;
+const os = std.os;
 
 // linux.getErrno(value) != .SUCCESS
 
-pub fn main() !void {
-    const bin = "/bin/sh";
-    const argv: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{ bin, "-i" };
+pub fn child(_: usize) callconv(.C) u8 {
+    const bin = "/bin/ls";
+    const argv: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{ bin, "-al" };
     const envp: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{};
     const newRoot = "/tmp/alpine";
-
-    var ret = linux.fork();
+    const ret = linux.chroot(newRoot);
     if (linux.E.init(ret) != .SUCCESS) {
+        std.debug.panic("chroot failed\n", .{});
+    }
+    std.debug.print("hey\n", .{});
+    _ = linux.execve(bin, argv, envp);
+    std.debug.print("panic", .{});
+    return 0;
+}
+
+pub fn main() !void {
+    var ptid: i32 = 0;
+    var ctid: i32 = 0;
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(general_purpose_allocator.deinit() == .ok);
+    const gpa = general_purpose_allocator.allocator();
+    const stack = try gpa.alloc(u8, 1024);
+    const pid = linux.clone(&child, @intFromPtr(&stack), 17, 0, &ptid, 0, &ctid);
+    if (linux.E.init(pid) != .SUCCESS) {
         std.debug.panic("panic\n", .{});
     }
 
-    if (ret != 0) { // parent
-        std.debug.print("{}\n", .{ret});
+    if (pid != 0) { // parent
+        std.debug.print("{}\n", .{pid});
         var status: u32 = undefined;
-        ret = linux.waitpid(-1, &status, 0);
-        if (linux.E.init(ret) != .SUCCESS) {
-            std.debug.panic("waitpid failed\n", .{});
+        const ret = linux.waitpid(-1, &status, 0);
+        const wait_err = std.posix.waitpid(-1, 0).status;
+        const err = linux.E.init(ret);
+        if (wait_err != 0) {
+            std.debug.panic("waitpid failed: {} {} \n", .{ err, ret });
         }
-    } else { // child
-        ret = linux.chroot(newRoot);
-        if (linux.E.init(ret) != .SUCCESS) {
-            std.debug.panic("chroot failed\n", .{});
-        }
-        std.debug.print("hey\n", .{});
-        _ = linux.execve(bin, argv, envp);
-        std.debug.print("panic", .{});
     }
 }
