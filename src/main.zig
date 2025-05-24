63a307a60dad3d41ec0298b179650e2f4fc1f498
Lorenzo Susini <susinilorenzo1@gmail.com>
2025-05-24 14:36:22 +0000

    fix: use SIGCHLD for waitpid to work and free memory
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,11 @@ const std = @import("std");
 const linux = std.os.linux;
 const os = std.os;
 
-// linux.getErrno(value) != .SUCCESS
+const SIGCHLD = 17;
 
 pub fn child(_: usize) callconv(.C) u8 {
-    const bin = "/bin/ls";
-    const argv: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{ bin, "-al" };
+    const bin = "/bin/sh";
+    const argv: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{ bin, "-i" };
     const envp: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{};
     const newRoot = "/tmp/alpine";
     const ret = linux.chroot(newRoot);
@@ -18,6 +18,7 @@ pub fn child(_: usize) callconv(.C) u8 {
         std.debug.panic("chroot failed\n", .{});
     }
     std.debug.print("hey\n", .{});
+    _ = linux.chdir("/");
     _ = linux.execve(bin, argv, envp);
     std.debug.print("panic", .{});
     return 0;
@@ -30,19 +31,18 @@ pub fn main() !void {
     defer std.debug.assert(general_purpose_allocator.deinit() == .ok);
     const gpa = general_purpose_allocator.allocator();
     const stack = try gpa.alloc(u8, 1024);
-    const pid = linux.clone(&child, @intFromPtr(&stack), 17, 0, &ptid, 0, &ctid);
+    const pid = linux.clone(&child, @intFromPtr(&stack), SIGCHLD, 0, &ptid, 0, &ctid);
     if (linux.E.init(pid) != .SUCCESS) {
         std.debug.panic("panic\n", .{});
     }
 
     if (pid != 0) { // parent
         std.debug.print("{}\n", .{pid});
-        var status: u32 = undefined;
-        const ret = linux.waitpid(-1, &status, 0);
-        const wait_err = std.posix.waitpid(-1, 0).status;
-        const err = linux.E.init(ret);
-        if (wait_err != 0) {
-            std.debug.panic("waitpid failed: {} {} \n", .{ err, ret });
+        const ret = linux.waitpid(-1, undefined, 0);
+        if (linux.E.init(ret) != .SUCCESS) {
+            std.debug.panic("waitpid failed\n", .{});
         }
+
+        gpa.free(stack);
     }
 }
