fc75d858e58076dfdd331892e4ad75c33c87df41
Antonio Pitasi <antonio@pitasi.dev>
2025-05-25 21:10:51 +0200

    fix memory leaks

--- src/cli.zig
+++ src/cli.zig
@@ -4,8 +4,17 @@ const testing = std.testing;
 const testallocator = testing.allocator;
 
 const RawArgs = struct {
+    allocator: Allocator,
+    args: [][:0]u8,
     executable_name: []u8,
     map: std.StringHashMap([]const u8),
+
+    const Self = @This();
+
+    pub fn deinit(self: *Self) void {
+        std.process.argsFree(self.allocator, self.args);
+        self.map.deinit();
+    }
 };
 
 const USAGE_STRING =
@@ -21,7 +30,7 @@ const USAGE_STRING =
 
 /// Print the usage message using the executable name.
 pub fn print_usage(args: RawArgs) void {
-    std.debug.print(USAGE_STRING, .{ args.executable_name });
+    std.debug.print(USAGE_STRING, .{args.executable_name});
 }
 
 fn arg_is_flag(arg: []u8) bool {
@@ -29,7 +38,7 @@ fn arg_is_flag(arg: []u8) bool {
 }
 
 /// Parse command-line arguments into a `RawArgs` structure.
-/// 
+///
 /// Arguments with values (e.g. `--something 8080`) are stored as key-value pairs.
 /// Flags not followed by anything (e.g `--something`) are stored with an empty value.
 ///
@@ -50,9 +59,11 @@ pub fn parseArgs(allocator: Allocator) !RawArgs {
     const args = try std.process.argsAlloc(allocator);
     if (args.len == 0) return error.MissingExecutable;
 
-    var result = RawArgs{ 
-        .map = std.StringHashMap(([]const u8)).init(allocator), 
-        .executable_name = args[0]
+    var result = RawArgs{
+        .allocator = allocator,
+        .args = args,
+        .map = std.StringHashMap([]const u8).init(allocator),
+        .executable_name = args[0],
     };
 
     if (args.len == 1) return result;
@@ -60,7 +71,7 @@ pub fn parseArgs(allocator: Allocator) !RawArgs {
         try result.map.put(args[1], "");
         return result;
     }
-    
+
     // skip the executable name and loop for each arg
     for (1..args.len - 1) |i| {
         const prev = args[i];
@@ -71,7 +82,7 @@ pub fn parseArgs(allocator: Allocator) !RawArgs {
             try result.map.put(prev, curr);
         }
     }
-    
+
     return result;
 }
 
@@ -95,6 +106,7 @@ pub fn get_i32(args: RawArgs, key: []const u8) i32 {
 pub fn print_raw_map(args: RawArgs) void {
     var iterator = args.map.iterator();
     while (iterator.next()) |key| {
-        std.debug.print("{s}: {s}\n", .{key.key_ptr.*, key.value_ptr.*});
+        std.debug.print("{s}: {s}\n", .{ key.key_ptr.*, key.value_ptr.* });
     }
 }
+
--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,7 @@ const cli = @import("cli.zig");
 
 const SIGCHLD = 17;
 
-pub fn child(_: usize) callconv(.C) u8 {    
+pub fn child(_: usize) callconv(.C) u8 {
     const bin = "/bin/sh";
     const argv: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{ bin, "-i" };
     const envp: [*:null]const ?[*:0]const u8 = &[_:null]?[*:0]const u8{};
@@ -18,7 +18,6 @@ pub fn child(_: usize) callconv(.C) u8 {
     if (linux.E.init(ret) != .SUCCESS) {
         std.debug.panic("chroot failed: {}\n", .{linux.E.init(ret)});
     }
-    std.debug.print("hey\n", .{});
     _ = linux.chdir("/");
     _ = linux.execve(bin, argv, envp);
     std.debug.print("panic", .{});
@@ -27,33 +26,34 @@ pub fn child(_: usize) callconv(.C) u8 {
 
 pub fn main() !u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    //defer std.debug.assert(gpa.deinit() == .ok);
+    defer std.debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
-    const args = cli.parseArgs(allocator) catch {
+
+    var args = cli.parseArgs(allocator) catch {
         std.debug.panic("Invalid args or an error occurred", .{});
     };
+    defer args.deinit();
 
     if (cli.get_bool(args, "--help")) {
         cli.print_usage(args);
         return 0;
     }
-    
+
     var ptid: i32 = 0;
     var ctid: i32 = 0;
     const stack = try allocator.alloc(u8, 1024);
+    defer allocator.free(stack);
     const pid = linux.clone(&child, @intFromPtr(&stack), SIGCHLD, 0, &ptid, 0, &ctid);
     if (linux.E.init(pid) != .SUCCESS) {
         std.debug.panic("panic\n", .{});
     }
 
     if (pid != 0) { // parent
-        std.debug.print("{}\n", .{pid});
+        std.time.sleep(100);
         const ret = linux.waitpid(-1, undefined, 0);
         if (linux.E.init(ret) != .SUCCESS) {
             std.debug.panic("waitpid failed\n", .{});
         }
-
-        allocator.free(stack);
     }
 
     return 0;
